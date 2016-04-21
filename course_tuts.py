17168c053b04a868318d7e69c8f7aaf28fc875c3
joshgrib <jgribbon@stevens.edu>
2016-04-20 21:25:49 -0400

    Commiting changes to delete local version

--- course_tuts.py
+++ course_tuts.py
@@ -1,5 +1,87 @@
 import csv
-if __name__ == '__main__':
+
+#Some good data to test with
+#Now I don't have to deal with the 30 minute times though!
+times = [['5-7', '6-9', '8-11', '9-11'],\
+    ['2-4', '4-7', '6-8', '7-9', '9-11'],\
+    ['2-4', '3-5', '4-6', '7-9', '9-11'],\
+    ['2-4', '4-6', '4-7', '7-9', '9-11'],\
+    ['2-4', '2-5', '4-7', '6-8', '7-9', '9-11'],\
+    ['1-3', '12-2', '2-5', '3-5']]
+times2 = [['5-7', '7-9', '8-11', '9-11'],\
+    ['2-4', '3-5', '4-6', '4-7', '6-8', '6-9', '7-9', '9-11'],\
+    ['2-4', '3-6', '4-6', '5-7', '6-9', '7-9', '9-11'],\
+    ['4-6', '4-7', '5-7', '7-9', '9-11'],\
+    ['2-4', '2-5', '4-6', '4-7', '5-7', '6-8', '7-9', '9-11'],\
+    ['1-3', '12-2', '2-5', '3-5']]
+
+def convert_time_24(time):
+    """
+    Given a number, convert to 24 H unless it's the hour 12
+    """
+    if not time == "12":
+        time = str(int(time) + 12)
+    return time
+def convert_time_12(time):
+    """
+    Given a number, convert to 12 H unless its the hour 12-2
+    """
+    if not time == "12":
+        time = str(int(time)-12)
+    return time
+def convert_times_24(lst):
+    """
+    Take a list of time periods in 12H format and covnert to 24H
+    """
+    for t_period in lst:
+        times = t_period.split('-')#get a 2-element array of the start and end of the time period
+        for time in times:#for both the start and end...
+            times[times.index(time)] = convert_time_24(time)
+        lst[lst.index(t_period)] = times[0] + '-' + times[1]
+    return lst
+def convert_times_12(lst):
+    """
+    Take a list of time periods in 24H format and convert to 12H
+    """
+    for t_period in lst:
+        times = t_period.split('-')#get a 2-element array of the start and end of the time period
+        for time in times:#for both the start and end...
+            times[times.index(time)] = convert_time_12(time)
+        lst[lst.index(t_period)] = times[0] + '-' + times[1]
+    return lst
+def combine_times_24(lst):
+    """
+        Take a list of times that may not be in order, convert to 24H
+        Then sort elements and combine as many as possible
+    """
+    for t_period in sorted(convert_times_24(lst)):
+        print t_period
+    print "---"
+
+def consolidate(lst):
+    """
+    Given a list of time periods, return the list of the consolidated times
+    """
+    lst = convert_times_24(lst)
+    start = lst[0].split("-")[0]
+    end = lst[0].split("-")[-1]
+    resp = []
+    for x in range(1, len(lst)):
+        if len(lst) == 1:
+            return convert_times_12(lst)
+        if lst[x].split("-")[0] <= end:#we can include this
+            end = lst[x].split("-")[-1]
+            if x+1 == len(lst):#if it's the end then add the last part
+                new = start + "-" + end
+                resp = resp + [new]
+        else:#time to make a new one
+            new = start + "-" + end
+            resp = resp + [new]
+            start = lst[x].split("-")[0]
+            end = lst[x].split("-")[-1]
+    return convert_times_12(resp)
+
+def run():
     data = {}
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
@@ -82,17 +164,17 @@ if __name__ == '__main__':
 
             #Fill the cells with the sorted times
             if 'Sunday' in result[course]['times']:
-                cell3 = sorted(result[course]['times']['Sunday'])
+                cell3 = consolidate(sorted(result[course]['times']['Sunday']))
             if 'Monday' in result[course]['times']:
-                cell4 = sorted(result[course]['times']['Monday'])
+                cell4 = consolidate(sorted(result[course]['times']['Monday']))
             if 'Tuesday' in result[course]['times']:
-                cell5 = sorted(result[course]['times']['Tuesday'])
+                cell5 = consolidate(sorted(result[course]['times']['Tuesday']))
             if 'Wednesday' in result[course]['times']:
-                cell6 = sorted(result[course]['times']['Wednesday'])
+                cell6 = consolidate(sorted(result[course]['times']['Wednesday']))
             if 'Thursday' in result[course]['times']:
-                cell7 = sorted(result[course]['times']['Thursday'])
+                cell7 = consolidate(sorted(result[course]['times']['Thursday']))
             if 'Friday' in result[course]['times']:
-                cell8 = sorted(result[course]['times']['Friday'])
+                cell8 = consolidate(sorted(result[course]['times']['Friday']))
             #write the row with all the info
             writer.writerow([ cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8 ])
 
@@ -101,3 +183,10 @@ if __name__ == '__main__':
         #    print "    " + day + str(result[course]['times'][day])
 
 
+if __name__ == '__main__':
+    run()
+#    for time in times:
+#        #combine_times(time)
+#        print time
+#        print consolidate(time)
+#        print "---"
